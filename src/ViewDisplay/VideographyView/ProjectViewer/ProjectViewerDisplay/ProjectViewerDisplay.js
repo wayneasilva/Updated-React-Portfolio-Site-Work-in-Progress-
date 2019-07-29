5fe077c1027163ee089b6cf5d99afa2391ccaf10
sunflowerSamurai <wayneasilva@gmail.com>
2019-07-29 17:47:52 -0400

    minor styling changes and bug fixes

--- src/ViewDisplay/VideographyView/ProjectViewer/ProjectViewerDisplay/ProjectViewerDisplay.js
+++ src/ViewDisplay/VideographyView/ProjectViewer/ProjectViewerDisplay/ProjectViewerDisplay.js
@@ -5,10 +5,25 @@ const projectViewerDisplay = (props) => {
     
     const currentView = props.portItems.filter( portItem => portItem.isLoaded === true);
 
+    const style = {
+        containerDiv: {
+            width: '100%',
+            backgroundColor: 'yellow'
+        },
+
+        viewH1: {
+            backgroundColor: 'red'
+        },
+
+        viewP: {
+            backgroundColor: 'blue'
+        }
+    }
+
     return (
-        <div>
-            <h1>{currentView[0].name}</h1>
-            <p>{currentView[0].about}</p>
+        <div style={style.containerDiv}>
+            <h1 style={style.viewH1}>{currentView[0].name}</h1>
+            <p style={style.viewP}>{currentView[0].about}</p>
         </div>
     )
 }
